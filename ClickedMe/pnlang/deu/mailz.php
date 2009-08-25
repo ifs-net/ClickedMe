7615b1561c1fa855166b8acec659700fcaf069ac
quan <quan@b8a9611b-6a28-0410-8186-d8a2cd10d6b2>
2009-08-25 14:57:53 +0000

    fixed #6 and adds mailz plugin

---
+++ ClickedMe/pnlang/deu/mailz.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @package      ClickedMe
+* @version      $Id: $
+* @author       Florian Schießl
+* @link         http://www.ifs-net.de
+* @copyright    Copyright (C) 2009
+* @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*/
+
+define('_CLICKEDME_LAST_VISITS', 'Letzte Besucher des eigenen Profils');
+define('_CLICKEDME_LAST_VISITS_DESCRIPTION', 'Dieses Plugin blendet ein, wer zuletzt auf dem eigenen Profil zu Besuch war.');
