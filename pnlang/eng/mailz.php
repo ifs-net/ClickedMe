9c953b84d87ea7af33eae01af0e65f260d775ce9
quan <quan@b8a9611b-6a28-0410-8186-d8a2cd10d6b2>
2009-08-30 19:01:33 +0000

    fixed #6 and adds mailz plugin

---
+++ pnlang/eng/mailz.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @package      ClickedMe
+* @version      $Id: mailz.php 32 2009-08-25 14:58:39Z quan $
+* @author       Florian Schießl
+* @link         http://www.ifs-net.de
+* @copyright    Copyright (C) 2009
+* @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*/
+
+define('_CLICKEDME_LAST_VISITS', 'Show last visitors for own profile');
+define('_CLICKEDME_LAST_VISITS_DESCRIPTION', 'This plugin displays the last visitors of the own profile page. Parameter show can be used for handling how many items should be shown. Standard value is set to 5.');
