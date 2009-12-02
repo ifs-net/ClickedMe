5303dd3b672528a6e40e04399ea6c073789e1acd
quan <quan@b8a9611b-6a28-0410-8186-d8a2cd10d6b2>
2009-12-02 20:25:27 +0000

    fixes #7 and adds myprofile plugin to clickedme module... thanks to Markus Größing for his code.

---
+++ pnlang/deu/myprofile.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @package      ClickedMe
+* @version      $Id: user.php 28 2009-04-29 14:15:08Z quan $
+* @author       Florian Schießl
+* @link         http://www.ifs-net.de
+* @copyright    Copyright (C) 2008
+* @license      http://www.gnu.org/copyleft/gpl.html GNU General Public License
+*/
+
+define('_CLICKEDMYTABTITLE', 'eigene Besucher');
+
+?>
