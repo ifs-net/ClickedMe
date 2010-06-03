71d4e729c0d6ff5e5c658d5cf915c8fe02d71db3
quan <quan@b8a9611b-6a28-0410-8186-d8a2cd10d6b2>
2010-06-03 20:44:50 +0000

    code cleanup

--- ClickedMe/pntables.php
+++ ClickedMe/pntables.php
@@ -9,39 +9,45 @@
 */
 
 /**
- * Populate pntables array for ClickedMe module
+ * Populate tables array for ClickedMe module
  * @return       array       The table information.
  */
 function ClickedMe_pntables()
 {
     // Initialise table array
-    $pntable = array();
+    $table = array();
 
     // Get the name for the tables
     $ClickedMe = DBUtil::getLimitedTableName('clickedme');
     $ClickedMe_Settings = DBUtil::getLimitedTableName('clickedme_settings');
 
     // Set the table name
-    $pntable['clickedme'] = $ClickedMe;
-    $pntable['clickedme_settings'] = $ClickedMe_Settings;
+    $table['clickedme'] = $ClickedMe;
+    $table['clickedme_settings'] = $ClickedMe_Settings;
 
     // Set the column names. 
-    $pntable['clickedme_column'] = array(	'id'		=> 'pn_id',
-						'uid'		=> 'pn_uid',
-						'clicked_uid'	=> 'pn_clicked_uid',
-						'timestamp'	=> 'pn_timestamp'	);
+    $table['clickedme_column'] = array(	
+        'id'            => 'pn_id',
+        'uid'           => 'pn_uid',
+        'clicked_uid'   => 'pn_clicked_uid',
+        'timestamp'     => 'pn_timestamp'
+        );
 
-    $pntable['clickedme_column_def'] = array(	'id'		=> "I NOTNULL AUTO PRIMARY",
-						'uid'		=> "I NOTNULL DEFAULT 0",
-						'clicked_uid'	=> "I NOTNULL DEFAULT 0",
-						'timestamp'	=> "C(11) NOTNULL DEFAULT 0");
-
+    $table['clickedme_column_def'] = array(
+        'id'            => "I NOTNULL AUTO PRIMARY",
+        'uid'           => "I NOTNULL DEFAULT 0",
+        'clicked_uid'   => "I NOTNULL DEFAULT 0",
+        'timestamp'     => "C(11) NOTNULL DEFAULT 0"
+        );
    
-    $pntable['clickedme_settings_column'] = array(	'uid'	=> 'pn_uid');
+    $table['clickedme_settings_column'] = array(	
+        'uid'           => 'pn_uid'
+        );
     
-    $pntable['clickedme_settings_column_def'] = array(	'uid'	=> "I NOTNULL PRIMARY");
-						
+    $table['clickedme_settings_column_def'] = array(	
+        'uid'           => "I NOTNULL PRIMARY"
+        );
 
     // Return the table information
-    return $pntable;
+    return $table;
 }
